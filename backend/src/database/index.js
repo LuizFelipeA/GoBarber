f8873bcc656f22627f25acdbab43b973ba8ea531
L.Felipe <luiz.felipe.da.silva@avanade.com>
2020-08-09 15:46:28 -0300

    final adjustments

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -26,10 +26,10 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: true,
-    });
+    // this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+    //   useNewUrlParser: true,
+    //   useFindAndModify: true,
+    // });
   }
 }
 
